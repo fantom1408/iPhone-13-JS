aa48db6c325611c74332700068e691123a4f71d2
fantom1408 <52611340+fantom1408@users.noreply.github.com>
2022-01-24 22:29:04 +0300

    Add files via upload

--- scripts/accordeon.js
+++ scripts/accordeon.js
@@ -1,41 +1,38 @@
 const accordeon = () => {
   const chItems = document.querySelectorAll('.characteristics__item');
 
-
-         const closeActiveItem = (e) => {
+  const closeActiveItem = (e) => {
     chItems.forEach((clickedItem) => {
       const clickedChButton = clickedItem.querySelector(
-        ".characteristics__title"
+        '.characteristics__title'
       );
       const clickedChContent = clickedItem.querySelector(
-        ".characteristics__description"
+        '.characteristics__description'
       );
 
       if (
-        clickedChButton.classList.contains("active") &&
+        clickedChButton.classList.contains('active') &&
         !clickedChButton.isEqualNode(e.target)
       ) {
-        clickedChContent.style.height = "";
-        clickedChButton.classList.remove("active");
-        clickedChContent.classList.remove("open");
+        clickedChContent.style.height = '';
+        clickedChButton.classList.remove('active');
+        clickedChContent.classList.remove('open');
       }
     });
   };
-
 
   chItems.forEach((item) => {
     const chButton = item.querySelector('.characteristics__title');
     const chContent = item.querySelector('.characteristics__description');
 
     chButton.addEventListener('click', (e) => {
-      closeActiveItem(e)
+      closeActiveItem(e);
       if (chContent.classList.contains('open')) {
         chContent.style.height = '';
       } else {
-          
         chContent.style.height = chContent.scrollHeight + 'px';
       }
-      chButton.classList.toggle('active')
+      chButton.classList.toggle('active');
       chContent.classList.toggle('open');
     });
   });
